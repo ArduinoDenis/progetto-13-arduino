8e6bb8fadd6cc884e2a8092d627c7f3762c80c78
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-03-03 19:21:44 +0100

    Update servo-motore.ino

--- servo-motore.ino
+++ servo-motore.ino
@@ -1,71 +1,37 @@
-#include <Servo.h> //Inserire la libreria Servo
+#include <Servo.h> // Include the Servo library
 
- 
-
-Servo Servo1; //Il nome del servo è Servo1
-
- 
+Servo Servo1; // Declare Servo1 object
 
 void setup() {
-
-  Servo1.attach (9); //Il Servo1 è collegato al pin digitale 
-
+  Servo1.attach(9); // Attach Servo1 to digital pin 9
 }
 
- 
-
 void loop() {
-
-  Servo1.write (-180);  //Imposto la posizione a cui si deve spostare il Servo1
-
-  delay (500);
-
-  Servo1.write (-150);
-
-  delay (500);
-
-  Servo1.write (-120);
-
-  delay (500);
-
-  Servo1.write (-90);
-
-  delay (500);
-
-  Servo1.write (-60);
-
-  delay (500);
-
-  Servo1.write (-30);
-
-  delay (500);
-
-  Servo1.write (0);
-
-  delay (500);
-
-  Servo1.write (30);
-
-  delay (500);
-
-  Servo1.write (60);
-
-  delay (500);
-
-  Servo1.write (90);
-
-  delay (500);
-
-  Servo1.write (120);
-
-  delay (500);
-
-  Servo1.write (150);
-
-  delay (500);
-
-  Servo1.write (180);
-
-  delay (500);
-
+  // Move Servo1 to various positions with delays in between
+  Servo1.write(-180);
+  delay(500);
+  Servo1.write(-150);
+  delay(500);
+  Servo1.write(-120);
+  delay(500);
+  Servo1.write(-90);
+  delay(500);
+  Servo1.write(-60);
+  delay(500);
+  Servo1.write(-30);
+  delay(500);
+  Servo1.write(0);
+  delay(500);
+  Servo1.write(30);
+  delay(500);
+  Servo1.write(60);
+  delay(500);
+  Servo1.write(90);
+  delay(500);
+  Servo1.write(120);
+  delay(500);
+  Servo1.write(150);
+  delay(500);
+  Servo1.write(180);
+  delay(500);
 }
